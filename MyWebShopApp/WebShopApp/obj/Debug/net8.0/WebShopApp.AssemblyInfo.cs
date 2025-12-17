5185b4798136cce72e275dfc1f050106f94cd73d
Alexandraa2021PMG <a.todorova2021@pmg-pernik.com>
2025-12-17 09:08:16 +0200

    part 5

--- MyWebShopApp/WebShopApp/obj/Debug/net8.0/WebShopApp.AssemblyInfo.cs
+++ MyWebShopApp/WebShopApp/obj/Debug/net8.0/WebShopApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebShopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7898089f07fb7e5e85d32d4e2f9ae1785411c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebShopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebShopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
